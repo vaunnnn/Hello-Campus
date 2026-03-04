858fec7258de1f8bf8ceb1cbfb42965e81c1bedb
Sybel Yadeen <aquillasybelyadeen123@gmail.com>
2026-03-04 14:09:47 +0800

    Commit 1

--- app/src/main/java/com/example/hellocampus2/MainActivity.java
+++ app/src/main/java/com/example/hellocampus2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.hellocampus2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("ID: 21104436");
+        }
+
+        Button button = findViewById(R.id.btnChange);
+        final TextView myText = findViewById(R.id.dept_id);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myText.setText("Dr. Angie Ceniza");
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
